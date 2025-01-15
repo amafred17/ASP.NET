4ce0a0b7d1891c6671be3b87a5ecab6ab604b98b
Fred Amanyire <Fred.Amanyire@unoc.co.ug>
2025-01-15 15:40:01 +0300

    added new project for Htmlhelpers

---
+++ HtmlHelpersDemo/HtmlHelpersDemo/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HtmlHelpersDemo.Models;
+
+namespace HtmlHelpersDemo.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Create()
+        {
+            var model = new UserModel
+            {
+                Skills = new List<string> { "C#", "JavaScript", "Python", "Java" },
+                Country = "USA" // Default value for dropdown
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(UserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save the user model to the database or perform other actions
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ HtmlHelpersDemo/HtmlHelpersDemo/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿namespace HtmlHelpersDemo.Models
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public bool IsActive { get; set; }
+        public string Gender { get; set; }
+        public string Country { get; set; }
+        public List<string> Skills { get; set; }
+        public string HiddenField { get; set; }
+    }
+}
