2878453a6d8f3aa51629c107aa0a7150e404683d
Fred Amanyire <Fred.Amanyire@unoc.co.ug>
2025-01-15 15:00:17 +0300

    added new project for feedback form in ASP.NET

---
+++ FeedbackApp/FeedbackApp/Controllers/FeedbackController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FeedbackApp.Models;
+
+namespace FeedbackApp.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        // GET: Feedback/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Feedback/Index
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(Feedback feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save feedback to database or perform other actions
+                return RedirectToAction("Success");
+            }
+
+            // If validation fails, return to the feedback form with errors
+            return View(feedback);
+        }
+
+        // GET: Feedback/Success
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
