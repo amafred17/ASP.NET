93d9f6de65878909ad289f0b8f14578d91251311
Fred Amanyire <Fred.Amanyire@unoc.co.ug>
2025-01-15 14:30:10 +0300

    added new project for user registration

---
+++ UserRegistrationApp/UserRegistrationApp/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserRegistrationApp.Models;
+
+namespace UserRegistrationApp.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: User/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save user to database or perform other actions
+                return RedirectToAction("Success");
+            }
+
+            // If validation fails, return to the registration form with errors
+            return View(user);
+        }
+
+        // GET: User/Success
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ UserRegistrationApp/UserRegistrationApp/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserRegistrationApp.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and Confirm Password must match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Age is required.")]
+        [Range(21, 30, ErrorMessage = "Age must be between 21 and 30.")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "User ID is required.")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*\d).{7,20}$",
+            ErrorMessage = "User ID must be 7-20 characters long, contain at least one capital letter and one digit.")]
+        public string UserId { get; set; }
+    }
+}
